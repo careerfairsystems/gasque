57169b0902ec2220699cd5a0f893fcff185ff9dc
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-10-19 12:51:31 +0200

    add non-student and other to program

--- modules/reservations/client/controllers/reservations.client.controller.js
+++ modules/reservations/client/controllers/reservations.client.controller.js
@@ -17,7 +17,6 @@
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
-    vm.programs = ['Non-Student', 'Other'];
     vm.isMember = undefined;
 
     // Check if ispaying
@@ -81,6 +80,7 @@
     // Get all programs
     var programsSet = new Set(ProgramsService);
     vm.programs = Array.from(programsSet);
+    vm.programs.unshift('Non-Student', 'Other');
 
     $scope.checkValidity = function(resForm) {
       vm.validity = resForm;
